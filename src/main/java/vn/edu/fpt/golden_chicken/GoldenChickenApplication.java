d54466123645af4f9665fb6f0a054c41c96e7167
VoAnhBen <benva.ce190709@gmail.com>
2026-01-07 23:45:50 +0700

    first commit

---
+++ src/main/java/vn/edu/fpt/golden_chicken/GoldenChickenApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.fpt.golden_chicken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenChickenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldenChickenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/edu/fpt/golden_chicken/GoldenChickenApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.fpt.golden_chicken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenChickenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
