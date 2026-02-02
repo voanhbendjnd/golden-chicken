90af58aecf0c24c6182956eca2285070cbb210fc
kyanhkg778 <anhldk.ce190574@gmail.com>
2026-02-02 15:43:56 +0700

    view intro

--- src/main/java/vn/edu/fpt/golden_chicken/controllers/client/HomeController.java
+++ src/main/java/vn/edu/fpt/golden_chicken/controllers/client/HomeController.java
@@ -1,12 +1,20 @@
 package vn.edu.fpt.golden_chicken.controllers.client;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     @GetMapping({ "/", "/home" })
-    public String getHomePage() {
+    public String getHomePage(Model model) {
+        model.addAttribute("activePage", "home");
         return "client/home";
+    }
+
+    @GetMapping("/ve-jollibee")
+    public String getAboutUsPage(Model model) {
+        model.addAttribute("activePage", "about");
+        return "client/about";
     }
 }
