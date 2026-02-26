0a277c05ef8efc5411669e4d0b0b34869ea193c8
VoAnhBen <benva.ce190709@gmail.com>
2026-02-26 23:57:12 +0700

    update

--- src/main/java/vn/edu/fpt/golden_chicken/controllers/client/AuthController.java
+++ src/main/java/vn/edu/fpt/golden_chicken/controllers/client/AuthController.java
@@ -1,22 +1,34 @@
 package vn.edu.fpt.golden_chicken.controllers.client;
 
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import jakarta.servlet.http.HttpSession;
 import vn.edu.fpt.golden_chicken.domain.request.UserDTO;
+import vn.edu.fpt.golden_chicken.repositories.UserRepository;
+import vn.edu.fpt.golden_chicken.services.MailService;
 import vn.edu.fpt.golden_chicken.services.UserService;
-import vn.edu.fpt.golden_chicken.utils.exceptions.EmailAlreadyExistsException;
 
 @Controller
 public class AuthController {
     private final UserService userService;
+    private final UserRepository userRepository;
+    private final MailService mailService;
 
-    public AuthController(UserService userService) {
+    public AuthController(MailService mailService, UserService userService, UserRepository userRepository) {
         this.userService = userService;
+        this.mailService = mailService;
+        this.userRepository = userRepository;
     }
 
     @GetMapping("/login")
@@ -31,22 +43,57 @@ public class AuthController {
     }
 
     @PostMapping("/register")
-    public String register(@ModelAttribute("registerUser") UserDTO userRequest, BindingResult bindingResult) {
+    public String register(HttpSession session, @ModelAttribute("registerUser") UserDTO userRequest,
+            BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return "client/admin/register";
         }
-        try {
-            this.userService.register(userRequest);
-
-        } catch (EmailAlreadyExistsException ex) {
-            bindingResult.rejectValue("email", "CONFLICT", ex.getMessage());
+        // this.userService.register(userRequest);
+        if (this.userRepository.existsByEmail(userRequest.getEmail())) {
+            bindingResult.rejectValue("email", "CONFLICT", "Email already exists!");
             return "client/auth/register";
         }
-        return "redirect:/login";
+        var otp = this.userService.generateBase();
+        session.setAttribute("PENDING_USER", userRequest);
+        session.setAttribute("OTP_CODE", otp);
+        session.setAttribute("OTP_EMAIL", userRequest.getEmail());
+        session.setAttribute("OTP_EXPIRE", LocalDateTime.now().plusMinutes(5));
+        this.mailService.startOTP(userRequest.getEmail(), otp);
+        return "redirect:/verify";
     }
 
     @GetMapping("/access-deny")
     public String getAccessDenyPage() {
         return "client/auth/access-deny";
+    }
+
+    @GetMapping("/verify")
+    public String verifyPage() {
+        return "client/auth/verify";
+    }
+
+    @PostMapping("/verify")
+    @ResponseBody
+    public ResponseEntity<?> verify(@RequestBody Map<String, String> request, HttpSession session) {
+        var otp = request.get("otp");
+        var email = request.get("email");
+
+        var sOTP = (String) session.getAttribute("OTP_CODE");
+        var sEmail = (String) session.getAttribute("OTP_EMAIL");
+        var expire = (LocalDateTime) session.getAttribute("OTP_EXPIRE");
+
+        if (sOTP.equals(otp) && sEmail.equals(email)) {
+            UserDTO pendingUser = (UserDTO) session.getAttribute("PENDING_USER");
+
+            if (pendingUser != null) {
+                this.userService.register(pendingUser);
+                session.removeAttribute("PENDING_USER");
+                session.removeAttribute("OTP_CODE");
+                session.removeAttribute("OTP_EMAIL");
+
+                return ResponseEntity.ok(Map.of("success", true));
+            }
+        }
+        return ResponseEntity.badRequest().body("INVALID_DATA");
     }
 }
