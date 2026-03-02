0c444697d3e432cd4c18d38953619589e10fca90
Nguyen Huy <huynt.ce190721@gmail.com>
2026-03-02 13:36:15 +0700

    update

--- src/main/java/vn/edu/fpt/golden_chicken/repositories/VoucherRepository.java
+++ src/main/java/vn/edu/fpt/golden_chicken/repositories/VoucherRepository.java
@@ -1,14 +1,18 @@
 package vn.edu.fpt.golden_chicken.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import vn.edu.fpt.golden_chicken.domain.entity.Voucher;
 
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface VoucherRepository extends JpaRepository<Voucher, Long> {
-    List<Voucher> findByIsDeletedFalse();
+    Page<Voucher> findByIsDeletedFalse(Pageable pageable);
     @Query("""
             SELECT v FROM Voucher v
             WHERE v.isDeleted = false
