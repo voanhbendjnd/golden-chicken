4de873324d7f1f3da8c18c5b75301fc0fa88f85b
VoAnhBen <benva.ce190709@gmail.com>
2026-03-07 17:35:04 +0700

    DJND bug/fix interface home field data for home page

--- src/main/java/vn/edu/fpt/golden_chicken/controllers/client/HomeController.java
+++ src/main/java/vn/edu/fpt/golden_chicken/controllers/client/HomeController.java
@@ -1,6 +1,9 @@
 package vn.edu.fpt.golden_chicken.controllers.client;
 
 import com.turkraft.springfilter.boot.Filter;
+
+import java.util.List;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
@@ -9,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import vn.edu.fpt.golden_chicken.domain.entity.Product;
+import vn.edu.fpt.golden_chicken.domain.response.ResProduct;
 import vn.edu.fpt.golden_chicken.services.ProductService;
 
 @Controller
@@ -30,12 +34,21 @@ public class HomeController {
         var saucies = this.productService.fetchAllChickenSauce(spec, pageable);
         var combos = this.productService.fetchAllComboWithPagination(spec, pageable);
         var deserts = this.productService.fetchAllLowMeal(spec, pageable);
+        var newProducts = this.productService.fetchAllWithPaginationNewProduct(spec);
+        var bestSeller = this.productService.fetchAllWithPaginationBestSeller(spec);
         model.addAttribute("meta", data.getMeta());
         model.addAttribute("alls", data.getResult());
         model.addAttribute("originals", originals.getResult());
         model.addAttribute("deserts", deserts.getResult());
         model.addAttribute("saucies", saucies.getResult());
         model.addAttribute("combos", combos.getResult());
+        model.addAttribute("newProducts", newProducts.getResult());
+        List<ResProduct> bestSellerLists = (List<ResProduct>) bestSeller.getResult();
+        var bestFirst = bestSellerLists.subList(0, 6);
+        var bestSecond = bestSellerLists.subList(6, bestSellerLists.size());
+        model.addAttribute("firstBestSellers", bestFirst);
+
+        model.addAttribute("secondBestSellers", bestSecond);
         return "client/home.djnd";
     }
 }
